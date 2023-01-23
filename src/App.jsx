10080cbe93b9528c7d8caa0fd388d3aeb341aa82
MartinPerez2 <tinchoperez00@gmail.com>
2023-01-23 11:10:22 -0300

    mi primer commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 
-
 function App() {
 
 
   return (
     <div className="bg-img-background w-full h-screen bg-no-repeat bg-center bg-black">
       <h1 className="text-center pt-20 text-3xl text-blue-300">Bienvenidos Devs a...</h1>
+      <h1>Hola Martin</h1>
     </div>
   )
 }
