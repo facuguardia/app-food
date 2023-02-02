9a251ab43a6daab5d1fef8167845167947cc28a1
Camilo <camiloleite14@gmail.com>
2023-02-02 17:41:08 -0300

    "LandingPageV2"

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,16 +1,16 @@
 import React from "react";
-import imgLanding from "../assets/logo-1.png";
+import imgLanding from "../assets/logo-2.png";
 import { Link } from "react-router-dom";
 
 function Landing() {
   return (
-    <div className=" bg-gradient-to-b from-[#24282E] to-[#15181b] flex items-center flex-col h-screen w-screen">
+    <div className=" bg-gradient-to-b from-[#24282E] to-background flex items-center flex-col h-screen w-screen">
       <img src={imgLanding} alt="a" className="relative top-32 scale-75" />
-      <h2 className="relative text-center top-36 text-[#E58326] font-bold text-4xl">
-         ¡Delicious food at one click!
+      <h2 className="relative text-center top-36 text-secondary font-bold text-4xl">
+         ¡Comidas deliciosas a un solo click!
       </h2>
       
-        <button className="bg-[#E58326] shadow-sm shadow-[#15181b] hover:shadow-[#15181b] hover:shadow-lg rounded-xl py-4 w-1/2 absolute top-90% hover:top-89% transition-all duration-150 "><Link to="/login">
+        <button className="bg-secondary shadow-sm shadow-[#15181b] hover:shadow-[#15181b] hover:shadow-lg rounded-xl max-w-md py-4 w-1/2 absolute top-90% hover:top-89% transition-all duration-150 "><Link to="/login">
         Get started</Link>
         </button>
       
