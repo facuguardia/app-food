45b7b7ba686def785a094ae3e1a4d6ffef5447b8
Facundo Guardia <101948163+facuguardia@users.noreply.github.com>
2023-02-12 08:38:17 -0300

    connecting the cart button with the order page

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -7,6 +7,7 @@ import Login from "../page/Login";
 import ProtectedRoute from "../ProtectedRoute";
 import Register from "../page/Register";
 import LandingPage from "../page/LandingPage";
+import Order from "../page/Order";
 
 // Aca importamos el context
 import { AuthProvider } from "../context/AuthContext";
@@ -15,24 +16,25 @@ import { CartProvider } from "../context/CartContext";
 function App() {
   return (
     <div className="bg-img-background bg-center bg-cover h-screen w-full text-gray-300 flex">
-    <CartProvider>
-      <AuthProvider>
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route
-            exact
-            path="/home"
-            element={
-              <ProtectedRoute>
-                <Home />
-              </ProtectedRoute>
-            }
-          />
-        </Routes>
-      </AuthProvider>
-    </CartProvider>
+      <CartProvider>
+        <AuthProvider>
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route
+              exact
+              path="/home"
+              element={
+                <ProtectedRoute>
+                  <Home />
+                </ProtectedRoute>
+              }
+            />
+            <Route path="/order" element={<Order />} />
+          </Routes>
+        </AuthProvider>
+      </CartProvider>
     </div>
   );
 }
