072392aa2bbbba3fafb770f7649a1bddb69cf438
Facundo Guardia <101948163+facuguardia@users.noreply.github.com>
2023-02-15 19:26:53 -0300

    order-style

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -5,14 +5,13 @@ import Categorias from "../components/Categorias";
 import Search from "../components/Search";
 import Cards from "../container/Cards";
 import Footer from "../components/Footer";
-import Sidebar from '../components/Sidebar'
+import Sidebar from "../components/Sidebar";
 // Iconos
 import { BsCart3 } from "react-icons/bs";
 // Plantilla card
 import { food } from "../data";
 // Contexto
 import { CartContext } from "../context/CartContext";
-
 
 // const [home, setHome] = useState({
 //     Cards: [],
@@ -35,14 +34,13 @@ import { CartContext } from "../context/CartContext";
 //   })
 // }
 
-
 function Home() {
   // Estado del sidebar
   const [menu, setMenu] = useState(false);
-//manejador del estado de sidebar 
-  const handleMenu = () =>{
-    setMenu (!menu)
-  }
+  //manejador del estado de sidebar
+  const handleMenu = () => {
+    setMenu(!menu);
+  };
   // Contador del carrito
   const [cart, setCart] = useContext(CartContext);
   const quantity = cart.reduce((acc, curr) => {
@@ -50,84 +48,8 @@ function Home() {
   }, 0);
 
   return (
-    <> 
-     {/* boton Carrito */}
-          <Link
-        to="/order"
-        className="fixed right-3 bottom-4 text-3xl bg-black/20 p-3 rounded-full text-white z-50"
-      >
-        <BsCart3 />{" "}
-        {quantity > 0 ? (
-          <span className="absolute w-6 h-6 flex justify-center items-center bg-gray-400 rounded-full text-xl text-black font-semibold bottom-9 right-0.5">
-            {quantity}
-          </span>
-        ) : null}
-      </Link>
-    <div
-    className="bg-gradient-to-b  from-background to-[#24282E] w-screen min-h-screen overflow-x-hidden p-2"
-    onClick={() => {
-      if (menu) {
-        handleMenu();
-      }
-    }}
-  > 
-
-    <Search />
-
-    <Categorias  />
-
-    <Cards food={food} />
-
-    <Footer />
-  </div>
-  <Sidebar handleMenu={handleMenu} menu={menu}/>
- </>
-);
-}
-export default Home;
-
-  // )
-    // <>
-      {/* Boton Sidebar */}
-      {/* <button
-        onClick={() => setMenu(true)}
-        className={`absolute top-[422px] items-center shadow-md pt-3 pb-3 pr-.5 pl-0 rounded-r-3xl text-white text-3xl font-bold bg-secondary motion-safe:animate-pulse`}
-      >
-        <BiChevronRight />
-      </button> */}
-      {/* Contenedor Sidebar */}
-      {/* <div
-        className={`bg-secondary h-full fixed w-[20%] flex flex-col justify-between items-center p-5 pt-16 rounded-r-3xl z-50 transition-all duration-300 ${
-          menu ? "left-0" : "-left-full"
-        }`}
-      > */}
-        {/* Menu Sidebar */}
-        {/* <nav className="flex flex-col items-center gap-10 text-gray-300 text-4xl"> */}
-          {/* Item */}
-          {/* <Link to="/home">
-            <RiHome3Line />
-          </Link> */}
-          {/* Item */}
-          {/* <Link to="#">
-            <CgProfile />
-          </Link> */}
-          {/* Item */}
-          {/* <Link to="#">
-            <MdOutlineLocalOffer />
-          </Link> */}
-          {/* Item */}
-          {/* <Link to="#">
-            <MdOutlineFavoriteBorder />
-          </Link>
-        </nav> */}
-        {/* Exit */}
-        {/* <div className="text-gray-300 text-4xl font-medium ">
-          <Link to="/">
-            <RxExit />
-          </Link>
-        </div> */}
-        {/* Boton carrito */}
-      {/* </div>
+    <>
+      {/* boton Carrito */}
       <Link
         to="/order"
         className="fixed right-3 bottom-4 text-3xl bg-black/20 p-3 rounded-full text-white z-50"
@@ -138,21 +60,25 @@ export default Home;
             {quantity}
           </span>
         ) : null}
-      </Link> */}
-      {/* Contenedor Home */}
-      //  <div
-      //   className="bg-gradient-to-b  from-background to-[#24282E] w-screen min-h-screen overflow-x-hidden p-2"
-      //   onClick={() => setMenu(false)}
-      // > 
-      //   <Search />
+      </Link>
+      <div
+        className="bg-gradient-to-b  from-background to-[#24282E] w-screen min-h-screen overflow-x-hidden p-2"
+        onClick={() => {
+          if (menu) {
+            handleMenu();
+          }
+        }}
+      >
+        <Search />
 
-      //   <Categorias />
+        <Categorias />
 
-      //   <Cards food={food} />
+        <Cards food={food} />
 
-      //   <Footer />
-      // </div>
-    // </>
-//   );
-// }
-// export default Home;
+        <Footer />
+      </div>
+      <Sidebar handleMenu={handleMenu} menu={menu} />
+    </>
+  );
+}
+export default Home;
--- src/page/Order.jsx
+++ src/page/Order.jsx
@@ -1,11 +1,51 @@
-import React from 'react'
+import React, { useState } from "react";
+// Plantilla card
+import { food } from "../data";
+// Componentes
+import OrderItem from "../components/OrderItem";
+import Sidebar from "../components/Sidebar";
 
-function Order() {
+const Order = () => {
+  // Estado del sidebar
+  const [menu, setMenu] = useState(false);
+  //manejador del estado de sidebar
+  const handleMenu = () => {
+    setMenu(!menu);
+  };
   return (
-    <div className='from-background to-[#24282E] w-screen min-h-screen'>
-      <h1>Pedidos</h1>
-    </div>
-  )
-}
+    <>
+      {/* Sidebar */}
+      <Sidebar handleMenu={handleMenu} menu={menu} />
+      {/* Contenedor de Orden */}
+      <div
+        className="grid grid-cols-1 w-full p-5"
+        onClick={() => {
+          if (menu) {
+            handleMenu();
+          }
+        }}
+      >
+        {/* Titulo */}
+        <h1 className="text-2xl font-bold text-center">Orden</h1>
 
-export default Order
+        {/* Items */}
+        {food.map((item) => (
+          <OrderItem key={item.id} item={item} />
+        ))}
+
+        {/* Total a pagar */}
+        <div className="w-full h-[90px] pb-8 flex justify-end items-center gap-4">
+          <span className="text-2xl font-bold">Total a pagar</span>
+          <span className="text-2xl font-bold">$ 100</span>
+        </div>
+
+        {/* Boton de pago */}
+        <div className=" bg-secondary flex justify-center items-center rounded-full">
+          <h1 className="text-2xl font-bold text-white">Pagar</h1>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Order;
